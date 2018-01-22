fcc6f690b611ab76dd8b2c249840812f01cc9389
ZacharyRMcKee <zmckee@hawk.iit.edu>
2018-01-22 11:50:07 -0600

    added basic conversion functionality

--- app/src/main/java/com/example/zmckee/tempcalculator/MainActivity.java
+++ app/src/main/java/com/example/zmckee/tempcalculator/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.zmckee.tempcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void convertClicked(View view)
+    {
+        EditText input = findViewById(R.id.input);
+        RadioButton radFtoC = findViewById(R.id.FtoC);
+        RadioButton radCtoF = findViewById(R.id.CtoF);
+        TextView textOutput = findViewById(R.id.output);
+        double output;
+        double inputValue = Double.parseDouble(input.getText().toString());
+        boolean convertFtoC = radFtoC.isChecked();
+        boolean convertCtoF = radCtoF.isChecked();
+        if(convertFtoC)
+        {
+            output = (inputValue - 32.0) * (5.0/9.0);
+        }
+        else if (convertCtoF)
+        {
+            output = (inputValue*9.0/5.0) + 32.0;
+        }
+        else
+        {
+            output = 0xDEADBEEF;
+        }
+        String outputString = String.format("%.1f",output);
+        textOutput.setText(outputString);
     }
 }
